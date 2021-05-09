771cfb22b4137ac60bf08f5bf3b0e303b227d928
matheusprandini <matheusprandini.96@gmail.com>
2021-05-08 23:02:04 -0300

    Added gitignore

--- utils/JsonHandler.py
+++
@@ -1,8 +0,0 @@
-import json
-
-class JsonHandler(object):
-
-    @staticmethod
-    def read_json(jsonFilePath):
-        with open(jsonFilePath) as jsonFile:    
-            return json.load(jsonFile)
